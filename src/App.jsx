8f01b5b6880d91cb3ee35169d6d6b4a1d8acbad8
MISBAHULLL <misbahulmunir36843@gmail.com>
2025-08-09 12:46:37 +0700

    menambahkan styling

--- src/App.jsx
+++ src/App.jsx
@@ -73,6 +73,61 @@ function App() {
         }`}>
           {aiReady ? "Ai Ready" : "Waiting for Ai..."}
         </div>
+        <div className="w-full max-w-2xl bg-gradient-to-r from-gray-800/90
+        to-gray-700/90 backdroup-blur-md border border-gray-600 rounded-3xl p-6 shadow-2xl">
+          <textarea className="w-full h-40 p-4 bg-gray-700/80 border borde-gray-600 rounded-2xl
+          text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-fuchsia-400
+          transition duration-300 disabled:opacity-50 reesize-none shadow-x1
+          focus:shadow-fuchsia-700/70" placeholder="Enter text... (max 3000 characters)"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          disabled={!aiReady}
+          maxLength={3000}></textarea>
+
+          <div className="flex items-center justify-between mt-4">
+            <span className="text-sm text-gray-400">
+              {text.length} / 3000 characters
+            </span>
+          </div>
+
+          <div className="flex gap-3 mt-4">
+            <button className="flex-1 px-6 py-3 bg-gradient-to-r from-rose-500 to-purple-500
+            hover:opacity-80 text-white font-semibold rounded-2xl transition
+            disabled:opacity-50 disabled:cursor-not-allowed"
+            onClick={speakText}
+            disabled={!aiReady || loading || text.trim()}>
+              {loading ? (
+                <div className="flex items-center justify-center gap-2">
+                <div className="animate-spin w-4 h-4 border-2 border-white/30 border-t-white
+                rounded-full"></div>
+                Speaking...
+                </div>
+              ):(
+                <div className="flex items-center justify-center gap-2 cursor-pointer">
+                  Speak
+                </div>
+              )}
+            </button>
+
+              {currentAudio && (
+                <button className="px-6 py-3 bg-gradient-to-r from-gray-600 to-gray-700
+                hover:opacity-80 text-white font-semibold rounded-2xl border
+                border-neutral-500/30 transition cursor-pointer"
+                onClick={stopAudio}>
+                  Stop
+                </button>
+              )}
+          </div>
+
+          <div className="mt-6 space-y-4 text-white">
+            {error && (
+              <div className="p-4 bg-red-100 text-red-700 border border-red-300 rounded-2xl">
+                {error}
+              </div>
+            )}
+          </div>
+
+        </div>
       </div>
     </>
 );
